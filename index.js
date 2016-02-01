d760fca6f2d30967056deb0a000e6acfd52c84a9
Seth Isernhagen <seth.isernhagen@globality.com>
2016-01-31 20:17:27 -0800

    copy with src dest array

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ let copy = require('recursive-copy');
 let inputFolder = '/Users/seth/Documents/zzz';
 let outputFolder = '/Users/seth/Documents/zzz_output';
 
+let copyFolders = [{
+  src: '/Users/seth/Documents/zzz',
+  dest: '/Users/seth/Documents/zzz_output'
+}, {
+  src: '/Users/seth/Documents/zzz2',
+  dest: '/Users/seth/Documents/zzz_output2'
+}];
 
-
-copy(inputFolder, outputFolder, { overwrite: true })
-	.then(function(results) {
-		console.info('Copied ' + results.length + ' files');
-	})
-	.catch(function(error) {
-		console.error('Copy failed: ' + error);
-	});
+for(let copyFolder of copyFolders) {
+  copy(copyFolder.src, copyFolder.dest, { overwrite: true })
+    .then(function(results) {
+      console.info('Copied ' + results.length + ' files');
+    })
+    .catch(function(error) {
+      console.error('Copy failed: ' + error);
+    });
+}
