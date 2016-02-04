c92f7a28a054a48c055d51d267c5702fcc0a9ff3
Seth Isernhagen <seth.isernhagen@globality.com>
2016-02-03 19:01:18 -0800

    WIP - added args, trying to copy single file

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
+/* global process */
 'use strict';
 
 let Promise = require("bluebird");
 let copy = require('recursive-copy');
+let fs = require('fs');
 
-let copyFolders = [{
-  src: '/Users/seth/Documents/zzz',
-  dest: '/Users/seth/Documents/zzz_output'
-}, {
-  src: '/Users/seth/Documents/zzz2',
-  dest: '/Users/seth/Documents/zzz_output2'
-}];
+let isFolder = process.argv[2] === 'multi';
+let folderName = process.argv[3];
+let label = process.argv[4];
+let fileName = process.argv[5];
 
-let intervalMins = 5
+let dest = {
+  tv: '/Users/seth/Documents/zzz_output',
+  movie: '/Users/seth/Documents/zzz_output2'
+};
 
-setInterval(function() {
-  for(let copyFolder of copyFolders) {
-    copy(copyFolder.src, copyFolder.dest)
-      .then(function(results) {
-        console.info('Copied ' + results.length + ' files');
-      })
-      .catch(function(error) {
-        console.error('Copy failed: ' + error);
-      });
-  }
-}, 1000);
+console.log('fileName =', fileName,
+  '\nfolderName =', folderName,
+  '\nisfolder =', isFolder,
+  '\nlabel', label);
+
+// copy(folderName, dest[label])
+// .then(function(results) {
+//   console.info('Copied ' + results.length + ' files');
+// });
+
+fs.createReadStream(folderName + '/' + fileName).pipe(fs.createWriteStream(dest[label] + folderName));
+
+// setInterval(function() {
+//   for(let copyFolder of copyFolders) {
+//     copy(copyFolder.src, copyFolder.dest)
+//       .then(function(results) {
+//         console.info('Copied ' + results.length + ' files');
+//       })
+//       .catch(function(error) {
+//         console.error('Copy failed: ' + error);
+//       });
+//   }
+// }, 1000);
 
